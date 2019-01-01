a69cecdb5aa738e2844e47049a9230db3cee1e01
Zehua-Chen <peterchen06242000@outlook.com>
2019-01-01 15:33:14 -0600

    [core] Move Type to Separate File

---
+++ include/Json/Type.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+/**
+ * Type of the value
+ */
+enum class Type
+{
+    // Value is object
+    object,
+    // Value is array
+    array,
+    // Value is primitive
+    primitive,
+};
