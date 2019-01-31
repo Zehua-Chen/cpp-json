725554deaecde9bb10663a6320cea28f40bc295f
Zehua-Chen <peterchen06242000@outlook.com>
2019-01-31 00:07:43 -0600

    [core] Setup Assembler and Its Test Cases

--- include/Json/Assembler.hpp
+++ include/Json/Assembler.hpp
@@ -1,5 +1,13 @@
+#include "Json/Token.hpp"
+#include "Json/BasicValue.hpp"
+
+namespace json::assemble
+{
 template<typename CharT>
 class Assembler
 {
-    
+public:
+    void operator()(const json::token::Token<CharT> &token);
+    BasicValue<CharT> &&root();
 };
+}
---
+++ test/AssemblerTest.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+#include "Json/Assembler.hpp"
+
+TEST(AssemblerTest, Simple)
+{
+    
+}
