a0aade50d4d6d1ad9614eb41801ffc826f63cdc2
Zehua-Chen <peterchen06242000@outlook.com>
2019-02-01 12:07:19 -0600

    [core] Implement Simple Object Assembling

--- include/Json/Assembler.hpp
+++ include/Json/Assembler.hpp
@@ -1,5 +1,18 @@
-#include "Json/Token.hpp"
+//
+//  BasicValue.hpp
+//  CPP
+//
+//  Created by Zehua Chen on 1/31/19.
+//  Copyright © 2019 Zehua Chen. All rights reserved.
+//
+
 #include "Json/BasicValue.hpp"
+#include "Json/Token.hpp"
+#include <stack>
+#include <utility>
+#include <iostream>
+
+// Delcarations
 
 namespace json::assemble
 {
@@ -9,5 +22,112 @@ class Assembler
 public:
     void operator()(const json::token::Token<CharT> &token);
     BasicValue<CharT> &&root();
+
+private:
+    /**
+     * _VType stands for Value Type, "_" prefix denotes private type
+     */
+    using _VType = typename BasicValue<CharT>::Type;
+
+    struct _Scope
+    {
+        BasicValue<CharT> value;
+        std::basic_string<CharT> name;
+
+        _Scope(_VType type, std::basic_string<CharT> &&name);
+        
+        bool isAnonymous() const;
+    };
+
+    std::basic_string<CharT> _key;
+    std::stack<_Scope> _stack;
 };
+} // namespace json::assemble
+
+// Implementations
+
+namespace json::assemble
+{
+template<typename CharT>
+Assembler<CharT>::_Scope::_Scope(
+    _VType type, std::basic_string<CharT> &&name)
+    : value(type)
+    , name(std::move(name))
+{
 }
+
+template<typename CharT>
+bool Assembler<CharT>::_Scope::isAnonymous() const
+{
+    return name.empty();
+}
+
+// Assembler<CharT> Implementation
+
+template<typename CharT>
+void Assembler<CharT>::operator()(const json::token::Token<CharT> &token)
+{
+    using namespace json::token;
+
+    // TType stands for Token Type
+    using TType = typename Token<CharT>::Type;
+
+    switch (token.type)
+    {
+    case TType::beginObject:
+        _stack.emplace(_VType::object, std::move(_key));
+        break;
+    case TType::endObject:
+    {
+        // Cannot touch the last one, needs it to return the root
+        std::cout << static_cast<int>(_stack.top().value.type()) << std::endl;
+        if (_stack.size() > size_t(1))
+        {
+            
+        }
+        
+        break;
+    }
+    case TType::beginArray:
+        _stack.emplace(_VType::array, std::move(_key));
+        break;
+    case TType::endArray:
+        break;
+    case TType::key:
+        _key = token.data;
+        break;
+    case TType::value:
+    {
+        _Scope &top = _stack.top();
+        BasicValue<CharT> &topValue = top.value;
+        
+        switch (topValue.type())
+        {
+        case _VType::object:
+        {
+            BasicValue<CharT> value = makePrimitive();
+            value.string(token.data);
+            
+            topValue[_key] = value;
+            _key.clear();
+            break;
+        }
+        default:
+            break;
+        }
+        
+        break;   
+    }
+    case TType::comment:
+        break;
+    default:
+        break;
+    }
+}
+
+template<typename CharT>
+json::BasicValue<CharT> &&Assembler<CharT>::root()
+{
+    return std::move(_stack.top().value);
+}
+} // namespace json::assemble
--- test/AssemblerTest.cpp
+++ test/AssemblerTest.cpp
@@ -1,7 +1,49 @@
 #include "gtest/gtest.h"
 #include "Json/Assembler.hpp"
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+using namespace std::string_literals;
+
+using namespace json;
+using namespace json::token;
+using namespace json::assemble;
+
+// Alias TType stands for Token Type and is used to reduce column width
+using TType = Token<char>::Type;
+using VType = BasicValue<char>::Type;
 
 TEST(AssemblerTest, Simple)
 {
-    
+    // Simple object
+    {
+        vector<Token<char>> tokens{
+            { TType::beginObject },
+            { TType::key, "name" },
+            { TType::value, "billy" },
+            { TType::key, "age" },
+            { TType::value, "12" },
+            { TType::endObject },
+        };
+        
+        Assembler<char> assembler;
+        
+        for (const auto &token: tokens)
+        {
+            assembler(token);
+        }
+        
+        auto object = assembler.root();
+        
+        ASSERT_EQ(VType::object, object.type());
+        
+        ASSERT_TRUE(object.contains("name"));
+        EXPECT_EQ(object.get("name").string(), "billy"s);
+        
+        ASSERT_TRUE(object.contains("age"));
+        EXPECT_EQ(object.get("age").string(), "12"s);
+    }
 }
