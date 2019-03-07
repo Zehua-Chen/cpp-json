e041bf403c3e9aef47e3715bd2241ede5276ef70
Zehua-Chen <peterchen06242000@outlook.com>
2019-03-06 21:59:19 -0600

    [prototype] Handle a Subset of Escape Characters

--- prototype/lexer/StringLexer.py
+++ prototype/lexer/StringLexer.py
@@ -1,16 +1,50 @@
-class StringLexer:
+import enum
+
+
+class StringLexerNotImplementedException(Exception):
     
+    def __str__(self):
+        return "Escape sequence not implemented"
+
+
+class StringLexer:
+
+    class State(enum.Enum):
+        STRING = 0
+        ESCAPE = 1
+        HEX_DIGIS = 2
+
     def __init__(self, finish_letter: str):
         self.finish_letter = finish_letter
         self.is_completed = False
         self.buffer = ""
-        
+        self.state = StringLexer.State.STRING
+
     def take_letter(self, letter: str):
-        
-        if letter == self.finish_letter:
-            self.is_completed = True
-        else:
-            self.buffer += letter
-            
-    def to_string(self):
+
+        if self.state == StringLexer.State.STRING:
+            if letter == self.finish_letter:
+                self.is_completed = True
+            elif letter == "\\":
+                self.state = StringLexer.State.ESCAPE
+            else:
+                self.buffer += letter
+        elif self.state == StringLexer.State.ESCAPE:
+            if letter == "\"":
+                self.buffer += "\""
+                self.state = StringLexer.State.STRING
+            elif letter == "\\":
+                self.buffer += "\\"
+                self.state = StringLexer.State.STRING
+            elif letter == "n":
+                self.buffer += "\n"
+                self.state = StringLexer.State.STRING
+            elif letter == "u":
+                self.state = StringLexer.State.HEX_DIGIS
+            else:
+                raise StringLexerNotImplementedException()
+        elif self.state == StringLexer.State.HEX_DIGIS:
+            pass
+
+    def to_string(self) -> str:
         return self.buffer
--- prototype/main.py
+++ prototype/main.py
@@ -2,8 +2,10 @@ from lexer import Lexer, Token, LexerException
 
 
 # json = "{ 'a': true, 'b': false, 'c': null, 'd': -12.22e-2, 'e': 'as we can' }"
-json = "{ 'abc': 'eee' }"
+json = "{ 'abc': 'ee\\\\e' }"
 lexer = Lexer(json)
+
+print(json)
 
 while not lexer.is_finished:
     lexer.extract_token()
